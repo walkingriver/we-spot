d1694988952a905f6ce7b8320b33c16e9ee13fb8
Michael Callaghan <mikecallaghan67@gmail.com>
2022-02-03 20:39:58 -0500

    Changed the slug generation to be potentially less offensive

--- src/app/setup/setup.page.ts
+++ src/app/setup/setup.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { generateSlug } from 'random-word-slugs';
+import { generateSlug, RandomWordOptions, totalUniqueSlugs } from 'random-word-slugs';
 
 @Component({
   selector: 'app-setup',
   templateUrl: './setup.page.html',
   styleUrls: ['./setup.page.scss'],
 })
-export class SetupPage {
+export class SetupPage implements OnInit {
   selectedNumberOfSymbols = 6;
   validSymbols = [
     { label: '4 - Easy', value: 4 },
@@ -15,5 +15,18 @@ export class SetupPage {
     { label: '12 - Extreme', value: 12 },
   ];
   deck: number[][] = [];
-  slug = generateSlug();
+  options: RandomWordOptions<3> = {
+    format: 'kebab',
+    partsOfSpeech: ['noun', 'noun', 'noun'],
+  };
+  slug = '';
+
+  ngOnInit(): void {
+    this.generateSlug();
+  }
+
+  generateSlug() {
+    this.slug = generateSlug(3, this.options);
+    console.log(totalUniqueSlugs());
+  }
 }
